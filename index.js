c1f7d8306a3d6152e2331c6af7fad2a258d38a43
MyPocketTokenFoundation <81549493+LifeCoachRay@users.noreply.github.com>
2022-08-02 15:34:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+const PORT = process.env.PORT || 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+
+
+const newspapers = [
+
+    {
+        name: 'Smackdown',
+        address: 'https://www.wwe.com/shows/smackdown',
+        base: 'https://www.wwe.com/'
+    },
+    {
+        name: 'Raw',
+        address: 'https://www.wwe.com/shows/raw',
+        base: 'https://www.wwe.com/'
+    },
+    {
+        name: 'AEW',
+        address: 'https://www.allelitewrestling.com/aew-news',
+        base: 'https://www.allelitewrestling.com/'
+    }
+]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+         .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("results")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+
+        })
+})
+
+app.get('/wrestling', (req, res) => {
+    res.json('Welcome to my Wrestling News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("results")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
